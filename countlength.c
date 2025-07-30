531aa8daa5efb5b9834826f3c224554972b668ac
ayushraj06-er <ayushrajtelghi55@gmail.com>
2025-07-30 07:48:03 +0530

    Add files via upload

---
+++ countlength.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// void printString (char arr[]);
+int countlength (char arr []);
+int main(){
+    char name[100];
+    //fgets (name,100,stdin);
+    scanf("%s",name); //back space input nhi kar payega scanf
+    printf("length is : %d",countlength (name));
+    return 0;
+}
+int countlength(char arr[]){
+    int count=0;
+    for (int i=0; arr[i]!='\0';i++){
+    count++;
+    }
+    return count;
+ }
+// void printString (char arr[]){
+//     for ( int i=0;arr[i]!='\0';i++){
+//         printf("%c",arr[i]);
+//     }
+// }
